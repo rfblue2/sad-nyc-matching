ec2376f942507b8439e2fe9913aa30c0ff1ee501
Roland Fong <rfblue2@gmail.com>
2020-05-10 14:49:39 -0400

    Only deploy on master, run tests on all branches

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+docker login --username=rfblue2@gmail.com --password=$HEROKU_TOKEN registry.heroku.com
+docker build -t registry.heroku.com/sad-nyc-matching-staging/server .
+docker push registry.heroku.com/sad-nyc-matching-staging/server
 IMAGE_ID=$(docker inspect registry.heroku.com/sad-nyc-matching-staging/server --format={{.Id}})
 PAYLOAD='{"updates":[{"type":"web","docker_image":"'"$IMAGE_ID"'"}]}'
 curl -n -X PATCH https://api.heroku.com/apps/sad-nyc-matching-staging/formation \
